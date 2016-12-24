cd20752055879259534e921e99d9589d672a80f6
Stanislav Vorobiov <sheffmail@mail.ru>
2016-12-24 16:25:09 +0300

    vdp: fix kernel dirs

--- scripts/CMake/cmake_i386_linux_debug.sh
+++ scripts/CMake/cmake_i386_linux_debug.sh
@@ -2,4 +2,7 @@
 DIR_NAME=`basename ${PWD}`
 mkdir -p ../${DIR_NAME}_build_i386_debug
 cd ../${DIR_NAME}_build_i386_debug
-cmake -DCMAKE_BUILD_TYPE=Debug -D_ECLIPSE_VERSION=4.3 -G "Eclipse CDT4 - Unix Makefiles" ../${DIR_NAME}
+cmake -DCMAKE_BUILD_TYPE=Debug -D_ECLIPSE_VERSION=4.3 \
+-DKERNEL_SOURCE_DIR=$HOME/kernel-src/linux-`uname -r | cut -f 1 -d-` \
+-DKERNEL_BUILD_DIR=$HOME/kernel-src/linux-`uname -r | cut -f 1 -d-`/debian/build/build-generic \
+-G "Eclipse CDT4 - Unix Makefiles" ../${DIR_NAME}
